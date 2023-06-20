76c297e02a76e7fa287a5f149fc2248ab6520a5f
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-06-20 16:07:58 -0600

    Changed date format

--- javascript/index.js
+++ javascript/index.js
@@ -49,7 +49,7 @@ function updateCity(event) {
             <div class="city">
                 <div>
                     <h2>${cityName}</h2>
-                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                    <div class="date">${cityTime.format("MMMM D, YYYY")}</div>
                 </div>
                 <div class="time">${cityTime.format(
                   "h:mm:ss"
